05406de582f167094da542048e10bcbf6c4500c3
Emre ÇOLAK <colakkemre@gmail.com>
2023-05-31 12:35:22 +0300

    complete tests route jest tests

---
+++ src/routes/tests/__test__/create.test.ts
@@ -0,0 +1,40 @@
+import request from "supertest"
+import { app } from "../../../app"
+
+
+it('POST /courses/:courseId/tests should create a test', async () => {
+
+    const token = await auth()
+
+    const courseResponse = await request(app)
+        .post('/courses')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+            name: 'Test Course',
+            courseDetails: 'Test course details'
+        }).expect(201)
+
+
+    const testResponse = await request(app)
+        .post(`/courses/${courseResponse.body.id}/tests`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+            name: 'First Test',
+            date: '2023-06-31'
+        }).expect(201)
+
+
+    await request(app)
+        .delete(`/courses/${courseResponse.body.id}/tests/${testResponse.body.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(204)
+
+    await request(app)
+        .delete(`/courses/${courseResponse.body.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(204)
+
+
+})
